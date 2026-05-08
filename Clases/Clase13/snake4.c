5ba77ba2f9428c08f4540b3674335182c50d8c68
dapaoesaya-jpg <da_pao_esaya@unam.edu>
2026-05-08 19:01:31 +0000

    Actializacion de la carpeta classe13

---
+++ Clases/Clase13/snake4.c
@@ -0,0 +1,154 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+#define WIDTH 30
+#define HEIGHT 15
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define YELLOW "\033[33m"
+#define BLUE "\033[33m"
+#define RESET "\033[0m"
+
+#ifdef _WIN32
+  #include <conio.h>
+  #include <windows.h>
+#else
+  #include <termios.h>
+  #include <unistd.h>
+  #include <fcntl.h>
+#endif
+int snakeX, snakeY; // posición de la primera parte de la vibora
+int fruitX, fruitY;
+char direction='d';
+int score=0;
+
+int increaseScore(){
+    static int score=0;
+    score++;
+    return score;
+}
+void moveSnake (int *x, int *y, char dir){
+    switch(dir){
+        case 'w': case 'W': (*y)--; break;
+        case 's': case 'S': (*y)++; break;
+        case 'a': case 'A': (*x)--; break;
+        case 'd': case 'D': (*x)++; break;//para mover la serpiente
+    }
+}
+void clearScreen(){
+#ifdef _WIN32
+    system("cls");
+#else
+    system ("clear");
+#endif
+}
+#ifdef _WIN32
+char readInput(){
+    if(_kbhit()) return _getc();
+    retrun direction;
+}
+#else
+struct termios oldt;
+void enableRawMove(){
+    struct termios newt;
+    tcgetattr(STDIN_FILENO,&oldt);
+    newt=oldt;
+    newt.c_lflag&=~(ICANON|ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW,&newt);
+    fcntl(STDIN_FILENO,F_GETFL, O_NONBLOCK);
+}
+char readInput(){
+    struct termios oldt, newt;//se engarga de no presionar enter
+    int ch;
+    int oldf;
+    tcgetattr(STDIN_FILENO,&oldt);
+    newt=oldt;
+    newt.c_lflag &=~(ICANON||ECHO);
+    tcsetattr(STDERR_FILENO, TCSANOW,&newt);
+    oldf=fcntl(STDERR_FILENO, F_GETFL,0);
+    fcntl(STDERR_FILENO, F_GETFL,oldf| O_NONBLOCK);
+    ch=getchar();
+    tcsetattr(STDERR_FILENO, TCSANOW,&oldt);
+    fcntl(STDIN_FILENO, F_GETFL, oldf);
+    if (ch!=EOF)return ch;
+    return direction;
+}
+#endif
+
+/**
+ * Función que sirve  para iniciar el juego
+ */
+void setup(){
+    snakeX=WIDTH/2;
+    snakeY=HEIGHT/2;
+
+    fruitX = rand()%(WIDTH-2)+1;
+    fruitY = rand()%(HEIGHT-2)+1;
+
+}
+
+void draw(){
+    clearScreen();
+    for(int i=0; i<HEIGHT;i++){
+        for(int j=0; j< WIDTH; j++){
+            if(j==0 || i ==0 || i==HEIGHT-1 || j == WIDTH-1){
+                 printf(YELLOW"#");
+                 continue;
+            }
+            if(snakeX==j && snakeY==i){
+                printf(GREEN"O");
+                continue;
+            } 
+            if(fruitX==j && fruitY==i){
+                printf(RED "f");
+                continue;
+            } 
+            printf(RESET" ");
+        }
+        printf(RESET"\n");
+    }
+    printf(RESET"Score %d \n", score);
+}
+
+
+void logic(){
+    if (snakeX<0||snakeX>=WIDTH|| snakeY<0||snakeY>= HEIGHT){
+        printf("Game over\n");
+        #ifndef _WIN32
+        enableRawMove();
+        #endif
+        exit(0);
+    }
+    if(snakeX== fruitX && snakeY== fruitY){
+        fruitX= rand ()%(WIDTH-2)+1;
+        fruitY= rand ()%(HEIGHT-2)+1;
+        score=increaseScore();
+    } 
+}
+
+int main(){
+    char newDirection;
+    setup();
+    #ifndef _WIN32
+    enableRawMove();
+    #endif
+    while(true){
+        draw();
+       newDirection=readInput();
+       switch(newDirection){
+        case 'w': case 'W': case 'a': case 'A': case 'd': case 'D':
+        direction= newDirection;
+        break;
+       }
+        moveSnake(&snakeX,&snakeY, direction);
+        logic();
+        #ifdef _WIN32
+        sleep(150);
+        #else
+        usleep(300000);
+        #endif
+
+    }
+    return 0;
+}
