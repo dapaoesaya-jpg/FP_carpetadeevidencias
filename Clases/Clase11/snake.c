befdef38c9cd607cb86cc55139e98aef66dd7a2b
dapaoesaya-jpg <da_pao_esaya@unam.edu>
2026-06-10 21:58:00 +0000

    Actualización

--- Clases/Clase11/snake.c
+++ Clases/Clase11/snake.c
@@ -1,55 +1,73 @@
+// Clase 27_04_26
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#define WDTH 20
-#define HEIGHT 10
-int x,y;
-int fruitX, fruitY;
-int score;
-char imput;
+
+#define WIDTH 10
+#define HEIGHT 30
+
+
 /*
-*Funcion que sirve para iniciar el juego
+Variables globales: Existen para todas las funciones, se declaran fuera de la 
+función, cualquier cambio en el código se refleja para todo el programa.
 */
+
+int x, y; // Posición de la primera parte de la vibora 
+int fruitX, fruitY; // Posición de la fruta
+int score;
+char input; // Tecla en el Teclado 
+
+//Función que sirve para iniciar el juego
+
 void setup(){
-    x=WDTH/2;
-    y=HEIGHT/2;// Fijando la posiocion incial del juagdor 
-    fruitX=rand()%WDTH;
-    // rand genera un seudo numero alateorio
-    fruitY=rand()%HEIGHT;
-    score 0;
+    //Fijando la posicion inicial en el "centro"
+    x=WIDTH/2;
+    y=HEIGHT/2;
+
+    //"rand" genera un número pseudoaleatorio limitado al ancho y alto
+    fruitX = rand()%WIDTH;
+    fruitY = rand()%HEIGHT;
+
+    score = 0;
 }
+
 void draw(){
-    system ("clear");
-    for(int i=0; i<WDTH; i++){
-        for (int j=0; j<HEIGHT; j++){
-            if(j==0|| i==0||j==WDTH-1||i==HEIGHT-1) {
+    //"System" Sirve para ejecutar comandos de la terminal
+    system("clear");
+    for(int i=0; i<WIDTH; i++){
+        for(int j=0; j<HEIGHT; j++){
+            if(j==0 || i==0 || j==WIDTH-1 || i==HEIGHT-1){
                 printf("#");
                 continue;
             }
-            if(x==j&& y==i){
-                printf ("S");
+            if(x==j && y==i){
+                printf("s");
                 continue;
             }
-            if(x==j && y==i){
-                printf("S");
-            }
-            if(fruitX==j&&fruitY){
+            if(fruitX==j && fruitY==i){
                 printf("f");
+                continue;
             }
             printf(" ");
         }
         printf("\n");
     }
-    printf ("Score: %d\n", score);
-    printf ("Presiona W o S o D o A\n");
-}// lafuncion system ejectuara comandos de la terminal 
-void imputFunc(){
-
+    printf("Score: %d\n", score);
+    printf("Presiona W,S,D o A\n");
 }
+
+void inputFunc(){
+    //Lee un caracter de la entrada
+    input = getchar();
+    getchar();
+}
+
 int main(){
     setup();
-    while (true){
+    while(true){
         draw();
-        imputFunc();
+        inputFunc();
     }
+    return 0;
 }
